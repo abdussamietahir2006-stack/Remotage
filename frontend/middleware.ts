a87b419387506a78cbbb403f168e815fc425f332
tahirabdussamie-max <tahirabdussamie@gmail.com>
2026-06-04 19:44:59 +0500

    k

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,11 +1,3 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-  res.headers.set('x-pathname', req.nextUrl.pathname);
-  return res;
-}
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
-};
+import { NextResponse } from 'next/server';
+export function middleware() { return NextResponse.next(); }
+export const config = { matcher: ['/'] };
